513625ee7940b2653cbc25b2df7f13c8a0303bc3
ArunAriA <kasik228483895@gmail.com>
2024-11-30 17:55:37 +0200

    Ending

--- 30.11.2024 cw.cpp
+++ 30.11.2024 cw.cpp
@@ -2,12 +2,76 @@
 
 using namespace std;
 
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	cout << &a << ' ' << &b << endl;
+//
+//	int* ptr = nullptr;
+//
+//	ptr = &a;
+//}
+
+//void show(int* ptr, int size)
+//{
+//	for (int i = 0; i < size; i++)
+//	{
+//
+//	}
+//}
+//
+//int summa(int* ptr, int size)
+//{
+//	int s = 0;
+//	for (int i = 0; i < size; i++)
+//	{
+//		s += *(ptr + i);
+//
+//	}
+//	return s;
+//}
+
+
+//int main()
+//{
+//	
+//	int ar[] = {1, 2, 3, 4};
+//	int size = sizeof(ar) / sizeof(ar[0]);
+//
+//	int* ptr = ar;
+//
+//	show(ptr, size);
+//
+//	int sum = summa(ptr, size);
+//	cout << endl << sum << endl;
+//
+//
+//	/*cout << size << endl;
+//	cout << ar << endl;
+//	cout << &ar << endl;
+//	for (int i = 0; i < size; i++)
+//	{
+//		cout << &ar[i] << ' ';
+//	}*/
+//	
+//}
+
+
+char func(int* ptr)
+{
+	if (*ptr < 0)
+		return '-';
+	else if (*ptr > 0 && *ptr == 0)
+		return '+';
+}
+
+
 int main()
 {
-	int a = 3;
-	int b = 5;
-	cout << &a << ' ' << &b << endl;
+	int a;
+	cin >> a;
 
-	int* ptr = nullptr;
-
+	char as = func(&a);
+	cout << as << endl;
 }
